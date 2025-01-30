5228aeda09b09162ea3a8d3ed97ee8e2630bcea3
jersonandino <albeiro.andino@gmail.com>
2025-01-30 01:47:42 -0500

    fix cors

--- crimetweets/settings.py
+++ crimetweets/settings.py
@@ -154,14 +154,16 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://192.168.100.53:5173",
-    "http://localhost:3000",
-    "http://192.168.100.53:3000"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://192.168.100.53:5173",
+#     "http://localhost:3000",
+#     "http://192.168.100.53:3000"
+# ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
 
